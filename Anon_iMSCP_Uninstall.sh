80d95e3ca159572a19d2016f2951021e264221f1
Juan Velasquez <juanchoraf@users.noreply.github.com>
2017-02-28 02:29:57 -0500

    Update Anon_iMSCP_Uninstall.sh

--- Anon_iMSCP_Uninstall.sh
+++ Anon_iMSCP_Uninstall.sh
@@ -1,15 +1,39 @@
-printf '\n
+printf "\n
 ******************************************************************************************************\r
 ************************************** AnonPro i-MSCP Uninstall **************************************\r
-******************************************************************************************************\r
-\n'
-echo " > Linux Header: $(uname -a)"
-echo " > Linux Version: $(uname -r)"
-echo " > Processor Type: $(uname -m)"
-echo ' > Operative System: '
-echo " $(lsb_release -a)"
-printf "\n [AnonB0T] I'm going to clean your system, this could take some minutes... \n [AnonB0T] Go for a cup of coffee :D \n"
-sleep 5s
+******************************************************************************************************\n
+ [AnonB0T] I'm going to clean your system, this could take some minutes... \r
+ [AnonB0T] Go for a cup of coffee :D \n
+   [info] Linux Header: %s\r
+   [info] Linux Version: %s\r
+   [info] Processor Type: %s\r
+   [info] Operative System: \n%s\n
+   [info] Stopping Services...\n\n" "$(uname -a)" "$(uname -r)" "$(uname -m)" "$(lsb_release -a)"
+sleep 4s
+
+service imscp* stop
+service php* stop
+service mysql* stop
+service dovecot* stop
+service postfix* stop
+service apache* stop
+service nginx* stop
+service proftpd* stop
+service fail2ban* stop
+service bind9* stop
+
+T1='   [info] %s services are running, PIDs are %s\n';
+T2='   [info] %s services goes down!\n';
+if P=$(pgrep imscp*); then printf "$T1" 'iMSCP' "$(pgrep imscp* | tr '\n\r' ' ')"; else printf "$T2" 'iMSCP'; fi
+if P=$(pgrep php*); then printf "$T1" 'Php' "$(pgrep php* | tr '\n\r' ' ')"; else printf "$T2" 'Php'; fi
+if P=$(pgrep mysql*); then printf "$T1" 'Mysql' "$(pgrep mysql* | tr '\n\r' ' ')"; else printf "$T2" 'Mysql'; fi
+if P=$(pgrep dovecot*); then printf "$T1" 'Dovecot' "$(pgrep dovecot* | tr '\n\r' ' ')"; else printf "$T2" 'Dovecot'; fi
+if P=$(pgrep postfix*); then printf "$T1" 'Postfix' "$(pgrep postfix* | tr '\n\r' ' ')"; else printf "$T2" 'Postfix'; fi
+if P=$(pgrep apache*); then printf "$T1" 'Apache' "$(pgrep apache* | tr '\n\r' ' ')"; else printf "$T2" 'Apache'; fi
+if P=$(pgrep nginx*); then printf "$T1" 'Nginx' "$(pgrep nginx* | tr '\n\r' ' ')"; else printf "$T2" 'Nginx'; fi
+if P=$(pgrep proftpd*); then printf "$T1" 'Proftpd' "$(pgrep proftpd* | tr '\n\r' ' ')"; else printf "$T2" 'Proftpd'; fi
+if P=$(pgrep fail2ban*); then printf "$T1" 'Fail2Ban' "$(pgrep fail2ban* | tr '\n\r' ' ')"; else printf "$T2" 'Fail2Ban'; fi
+if P=$(pgrep bind9*); then printf "$T1" 'Bind9' "$(pgrep bind9* | tr '\n\r' ' ')"; else printf "$T2" 'Bind9'; fi
 
 apt-get -y remove --purge dovecot* && apt-get -y autoremove && apt-get -y autoclean
 apt-get -y remove --purge postfix* && apt-get -y autoremove && apt-get -y autoclean
